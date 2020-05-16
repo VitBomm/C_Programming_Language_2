9ffaaed7bb74752b61a0fc1568de5113d32e168b
Vit <nnphucthinh@gmail.com>
2020-05-16 11:56:35 +0700

    Fahrenit To Celisusi with float

--- fahrenheit-to-celsius.c
+++ fahrenheit-to-celsius.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
@@ -10,8 +10,8 @@ int main() {
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr += step;
     }
 }
